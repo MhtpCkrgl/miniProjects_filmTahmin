3822cb2aa488a894670705340d33480eff664d4e
Mehtap <mhtpckrgl@gmil.com>
2023-06-08 12:11:03 +0300

    v1

---
+++ FilmTahmin/Depo.java
@@ -0,0 +1,26 @@
+package practice.FilmTahmin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Depo {
+     /*
+     TASK :
+      yukarıdaki film listinde bulunan filmlerden herhangi birisini kullanıcının film ismini  görmeden
+      filmin index nosuna göre sectiriniz
+      seçilen filmin  ismini filmin harf saysının 2 katı kadar hak tanıyarak tahmin etmesini
+      sağlayan method create ediniz...
+      kullanıcının sectiği filmin harf sayısını  konsola yazdırınız.
+      kullanıcının sectiği film için tahmin hakkını  konsola yazdırınız.
+      kullanıcının her tahmininde kalan hak sayısını konsola yazdırınız.
+      kullanıcının her tahmininde doğru ve yanlış tahmin  sayısını konsola yazdırınız.
+      kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  konsola yazdırınız.
+
+     */
+    static Scanner scan=new Scanner(System.in);
+    static List<String> film=new ArrayList<>(List.of("JOKER","INTERSTALLER"
+            ,"AVATAR","HABABAM SINIFI","SONIC","TITANIC","INCEPTION","ZELIHA","AYLA","SAKAR SAKIR"));
+
+
+}
---
+++ FilmTahmin/Runner.java
@@ -0,0 +1,16 @@
+package practice.FilmTahmin;
+
+import java.util.Collections;
+
+import static practice.FilmTahmin.Methods.baslatMethod;
+
+public class Runner extends Depo {
+
+
+    public static void main(String[] args) {
+
+        startMethod();
+    }
+
+
+}
